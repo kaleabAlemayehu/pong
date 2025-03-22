5de3b4b90b8ff1d0bf00eada0306688f63657d3d
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-03-22 04:08:06 -0400

    refactor: restructure the game into its own struct and make game logic tide to it.

--- main.go
+++ main.go
@@ -11,11 +11,6 @@ const SCREEN_WIDTH int32 = 800
 const SCREEN_HEIGHT int32 = 450
 const SCORE_LIMIT int32 = 3
 
-type score struct {
-	red  int32
-	blue int32
-}
-
 type Ball struct {
 	Position rl.Vector2
 	Speed    rl.Vector2
@@ -29,147 +24,155 @@ type Player struct {
 	Score    int32
 }
 
-var RED Player = Player{
-	Position: rl.Vector2{
-		X: 0,
-		Y: 200,
-	},
-	Size: rl.Vector2{
-		X: 10,
-		Y: 100,
-	},
+type Game struct {
+	Red  Player
+	Blue Player
+	Ball Ball
 }
 
-var BLUE Player = Player{
-	Position: rl.Vector2{
-		X: float32(SCREEN_WIDTH) - 10,
-		Y: 200,
-	},
-	Size: rl.Vector2{
-		X: 10,
-		Y: 100,
-	},
-}
+func NewGame() *Game {
 
-var ball Ball = Ball{
-	Position: rl.Vector2{
-		X: float32(SCREEN_WIDTH) / 2,
-		Y: float32(SCREEN_HEIGHT) / 2,
-	},
-	Speed: rl.Vector2{
-		X: 3.0,
-		Y: 0.0,
-	},
-	IsActive: false,
-}
+	return &Game{
+		Red: Player{
+			Position: rl.Vector2{
+				X: 0,
+				Y: 200,
+			},
+			Size: rl.Vector2{
+				X: 10,
+				Y: 100,
+			},
+		},
 
-var SCORE score
+		Blue: Player{
+			Position: rl.Vector2{
+				X: float32(SCREEN_WIDTH) - 10,
+				Y: 200,
+			},
+			Size: rl.Vector2{
+				X: 10,
+				Y: 100,
+			},
+		},
+		Ball: Ball{
+			Position: rl.Vector2{
+				X: float32(SCREEN_WIDTH) / 2,
+				Y: float32(SCREEN_HEIGHT) / 2,
+			},
+			Speed: rl.Vector2{
+				X: 3.0,
+				Y: 0.0,
+			},
+			IsActive: false,
+		},
+	}
+}
 
 func main() {
 	rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "pong")
 	defer rl.CloseWindow()
 
+	g := NewGame()
 	rl.SetTargetFPS(60)
 	for !rl.WindowShouldClose() {
-		handleMovement()
-		if ball.IsActive {
-			ball.Position.X = ball.Position.X + float32(ball.Speed.X)
-			ball.Position.Y = ball.Position.Y + float32(ball.Speed.Y)
+		g.handleMovement()
+		if g.Ball.IsActive {
+			g.Ball.Position.X = g.Ball.Position.X + float32(g.Ball.Speed.X)
+			g.Ball.Position.Y = g.Ball.Position.Y + float32(g.Ball.Speed.Y)
 		} else {
-			ball.Position = rl.Vector2{X: RED.Position.X + 2*RED.Size.X + ball.Radius, Y: RED.Position.Y}
+			g.Ball.Position = rl.Vector2{X: g.Red.Position.X + 2*g.Red.Size.X + g.Ball.Radius, Y: g.Red.Position.Y}
 		}
 		// ball.Position.Y = ball.Position.Y + float32(ball.Speed.Y)
-		if ball.Position.X <= 0 {
-			BLUE.Score = BLUE.Score + 1
+		if g.Ball.Position.X <= 0 {
+			g.Blue.Score = g.Blue.Score + 1
 			// reset
-			ball.reset()
+			g.reset()
 		}
-		if ball.Position.X >= float32(SCREEN_WIDTH) {
-			RED.Score = RED.Score + 1
+		if g.Ball.Position.X >= float32(SCREEN_WIDTH) {
+			g.Red.Score = g.Red.Score + 1
 			// reset
-			ball.reset()
+			g.reset()
 		}
 
-		if ball.Position.Y <= 0 {
-			ball.Speed.Y = 3.0
+		if g.Ball.Position.Y <= 0 {
+			g.Ball.Speed.Y = 3.0
 		}
-		if ball.Position.Y >= float32(SCREEN_HEIGHT) {
-			ball.Speed.Y = -3.0
+		if g.Ball.Position.Y >= float32(SCREEN_HEIGHT) {
+			g.Ball.Speed.Y = -3.0
 		}
 
-		if rl.CheckCollisionCircleRec(ball.Position, ball.Radius, rl.Rectangle{X: RED.Position.X, Y: RED.Position.Y - RED.Size.Y/2, Width: RED.Size.X, Height: RED.Size.Y}) {
-			ball.Speed.X = 3.0
-			ball.Speed.Y = (ball.Position.Y - RED.Position.Y) / (RED.Size.Y / 2) * 5
+		if rl.CheckCollisionCircleRec(g.Ball.Position, g.Ball.Radius, rl.Rectangle{X: g.Red.Position.X, Y: g.Red.Position.Y - g.Red.Size.Y/2, Width: g.Red.Size.X, Height: g.Red.Size.Y}) {
+			g.Ball.Speed.X = 3.0
+			g.Ball.Speed.Y = (g.Ball.Position.Y - g.Red.Position.Y) / (g.Red.Size.Y / 2) * 5
 
 		}
-		if rl.CheckCollisionCircleRec(ball.Position, 10.0, rl.Rectangle{X: BLUE.Position.X, Y: BLUE.Position.Y - BLUE.Size.Y/2, Width: BLUE.Size.X, Height: BLUE.Size.Y}) {
-			ball.Speed.X = -3.0
-			ball.Speed.Y = (ball.Position.Y - BLUE.Position.Y) / (BLUE.Size.Y / 2) * 5
-			// INFO: ball.speed.x = (ball.position.x - player.position.x)/(player.size.x/2)*5;
+		if rl.CheckCollisionCircleRec(g.Ball.Position, 10.0, rl.Rectangle{X: g.Blue.Position.X, Y: g.Blue.Position.Y - g.Blue.Size.Y/2, Width: g.Blue.Size.X, Height: g.Blue.Size.Y}) {
+			g.Ball.Speed.X = -3.0
+			g.Ball.Speed.Y = (g.Ball.Position.Y - g.Blue.Position.Y) / (g.Blue.Size.Y / 2) * 5
 		}
 
 		// INFO: drawing start
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
-		rl.DrawRectangleRec(rl.Rectangle{X: RED.Position.X, Y: RED.Position.Y - (RED.Size.Y / 2), Width: RED.Size.X, Height: RED.Size.Y}, rl.Red)
-		rl.DrawRectangleRec(rl.Rectangle{X: BLUE.Position.X, Y: BLUE.Position.Y - (BLUE.Size.Y / 2), Width: BLUE.Size.X, Height: BLUE.Size.Y}, rl.Blue)
-		rl.DrawCircleV(ball.Position, 10.0, rl.White)
-		rl.DrawText(fmt.Sprintf("RED: %v |<=>| BLUE: %v", RED.Score, BLUE.Score), int32(rl.GetScreenWidth()/2)-115, 0, 20, rl.White)
+		rl.DrawRectangleRec(rl.Rectangle{X: g.Red.Position.X, Y: g.Red.Position.Y - (g.Red.Size.Y / 2), Width: g.Red.Size.X, Height: g.Red.Size.Y}, rl.Red)
+		rl.DrawRectangleRec(rl.Rectangle{X: g.Blue.Position.X, Y: g.Blue.Position.Y - (g.Blue.Size.Y / 2), Width: g.Blue.Size.X, Height: g.Blue.Size.Y}, rl.Blue)
+		rl.DrawCircleV(g.Ball.Position, 10.0, rl.White)
+		rl.DrawText(fmt.Sprintf("RED: %v |<=>| BLUE: %v", g.Red.Score, g.Blue.Score), int32(rl.GetScreenWidth()/2)-115, 0, 20, rl.White)
 		rl.EndDrawing()
 	}
 }
 
-func handleMovement() {
+func (g *Game) handleMovement() {
 	if rl.IsKeyDown(rl.KeyJ) {
-		if RED.Position.Y < float32(SCREEN_HEIGHT)-RED.Size.Y/2 {
-			RED.Position.Y = RED.Position.Y + 2
+		if g.Red.Position.Y < float32(SCREEN_HEIGHT)-g.Red.Size.Y/2 {
+			g.Red.Position.Y = g.Red.Position.Y + 2
 		}
 	}
 	if rl.IsKeyDown(rl.KeyK) {
-		if RED.Position.Y > RED.Size.Y/2 {
-			RED.Position.Y = RED.Position.Y - 2
+		if g.Red.Position.Y > g.Red.Size.Y/2 {
+			g.Red.Position.Y = g.Red.Position.Y - 2
 		}
 	}
 	if rl.IsKeyDown(rl.KeyH) {
-		if RED.Position.X > 0 {
-			RED.Position.X = RED.Position.X - 2
+		if g.Red.Position.X > 0 {
+			g.Red.Position.X = g.Red.Position.X - 2
 		}
 	}
 	if rl.IsKeyDown(rl.KeyL) {
-		if RED.Position.X < float32(SCREEN_WIDTH)-RED.Size.X {
-			RED.Position.X = RED.Position.X + 2
+		if g.Red.Position.X < float32(SCREEN_WIDTH)-g.Red.Size.X {
+			g.Red.Position.X = g.Red.Position.X + 2
 		}
 	}
 
 	if rl.IsKeyDown(rl.KeyA) {
-		if BLUE.Position.X > 0 {
-			BLUE.Position.X = BLUE.Position.X - 2
+		if g.Blue.Position.X > 0 {
+			g.Blue.Position.X = g.Blue.Position.X - 2
 		}
 	}
 
 	if rl.IsKeyDown(rl.KeyF) {
-		if BLUE.Position.X < float32(SCREEN_WIDTH)-BLUE.Size.X {
-			BLUE.Position.X = BLUE.Position.X + 2
+		if g.Blue.Position.X < float32(SCREEN_WIDTH)-g.Blue.Size.X {
+			g.Blue.Position.X = g.Blue.Position.X + 2
 		}
 	}
 
 	if rl.IsKeyDown(rl.KeyS) {
-		if BLUE.Position.Y < float32(SCREEN_HEIGHT)-BLUE.Size.Y/2 {
-			BLUE.Position.Y = BLUE.Position.Y + 2
+		if g.Blue.Position.Y < float32(SCREEN_HEIGHT)-g.Blue.Size.Y/2 {
+			g.Blue.Position.Y = g.Blue.Position.Y + 2
 		}
 	}
 	if rl.IsKeyDown(rl.KeyD) {
-		if BLUE.Position.Y > BLUE.Size.Y/2 {
-			BLUE.Position.Y = BLUE.Position.Y - 2
+		if g.Blue.Position.Y > g.Blue.Size.Y/2 {
+			g.Blue.Position.Y = g.Blue.Position.Y - 2
 		}
 	}
 	if rl.IsKeyDown(rl.KeySpace) {
-		ball.IsActive = true
+		g.Ball.IsActive = true
 	}
 }
 
-func (b *Ball) reset() {
-	if RED.Score >= SCORE_LIMIT {
+func (g *Game) reset() {
+	if g.Red.Score >= SCORE_LIMIT {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
 		rl.DrawText("RED WINS!!", int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 32, rl.Red)
@@ -178,7 +181,7 @@ func (b *Ball) reset() {
 		os.Exit(0)
 	}
 
-	if BLUE.Score >= SCORE_LIMIT {
+	if g.Blue.Score >= SCORE_LIMIT {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
 		rl.DrawText("BLUE WINS!!", int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 32, rl.Blue)
@@ -189,25 +192,25 @@ func (b *Ball) reset() {
 
 	rl.BeginDrawing()
 	rl.ClearBackground(rl.Black)
-	rl.DrawText(fmt.Sprintf("RED: %v |<=>| BLUE: %v", RED.Score, BLUE.Score), int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 24, rl.White)
+	rl.DrawText(fmt.Sprintf("RED: %v |<=>| BLUE: %v", g.Red.Score, g.Blue.Score), int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 24, rl.White)
 	rl.EndDrawing()
 	rl.WaitTime(2.5)
 
 	// INFO: reset blue
-	BLUE.Position.X = float32(SCREEN_WIDTH) - 10
-	BLUE.Position.Y = 200
-	BLUE.Size.X = 10
-	BLUE.Size.Y = 100
+	g.Blue.Position.X = float32(SCREEN_WIDTH) - 10
+	g.Blue.Position.Y = 200
+	g.Blue.Size.X = 10
+	g.Blue.Size.Y = 100
 
 	// INFO: reset red
-	RED.Position.X = 0
-	RED.Position.Y = 200
-	RED.Size.X = 10
-	RED.Size.Y = 100
+	g.Red.Position.X = 0
+	g.Red.Position.Y = 200
+	g.Red.Size.X = 10
+	g.Red.Size.Y = 100
 
 	// INFO: reset ball
-	ball.Position = rl.Vector2{X: RED.Position.X + 2*RED.Size.X + b.Radius, Y: RED.Position.Y}
-	b.Speed.Y = 0.0
-	b.Speed.X = 3.0
-	b.IsActive = false
+	g.Ball.Position = rl.Vector2{X: g.Red.Position.X + 2*g.Red.Size.X + g.Ball.Radius, Y: g.Red.Position.Y}
+	g.Ball.Speed.Y = 0.0
+	g.Ball.Speed.X = 3.0
+	g.Ball.IsActive = false
 }
