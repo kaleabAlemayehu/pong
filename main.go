c6b9e92745233042ea6f7dafea66b7bd5615420a
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-03-28 15:51:20 -0400

    feat: glue togther client and server logic
    making its own game struct
    create input and message channel to communicate with the client thread
    sending commands to input channel
    receive command through message channel and update the new game state of
    its with the new game state from the server

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"playground/raylib-go/client"
 	"playground/raylib-go/server"
 
@@ -15,89 +16,140 @@ const SCREEN_WIDTH int32 = 800
 const SCREEN_HEIGHT int32 = 450
 const SCORE_LIMIT int32 = 3
 
+type Ball struct {
+	Position rl.Vector2 `json:"position"`
+	Speed    rl.Vector2 `json:"speed"`
+	Radius   float32    `json:"radius"`
+	IsActive bool       `json:"is_active"`
+}
+
+type Player struct {
+	Position rl.Vector2 `json:"position"`
+	Size     rl.Vector2 `json:"size"`
+	Score    int32      `json:"score"`
+}
+
+type Game struct {
+	Red    Player         `json:"red"`
+	Blue   Player         `json:"blue"`
+	Ball   Ball           `json:"ball"`
+	Client *client.Client `json:"client"`
+}
+
+var g *Game = &Game{
+	Red: Player{
+		Position: rl.Vector2{
+			X: 0,
+			Y: 200,
+		},
+		Size: rl.Vector2{
+			X: 10,
+			Y: 100,
+		},
+	},
+
+	Blue: Player{
+		Position: rl.Vector2{
+			X: float32(SCREEN_WIDTH) - 10,
+			Y: 200,
+		},
+		Size: rl.Vector2{
+			X: 10,
+			Y: 100,
+		},
+	},
+	Ball: Ball{
+		Position: rl.Vector2{
+			X: float32(SCREEN_WIDTH) / 2,
+			Y: float32(SCREEN_HEIGHT) / 2,
+		},
+		Speed: rl.Vector2{
+			X: 3.0,
+			Y: 0.0,
+		},
+		IsActive: false,
+	},
+}
+
 func main() {
 	host := flag.Bool("host", false, "to host server of the game")
 	flag.Parse()
+	message := make(chan []byte, 10000)
+	input := make(chan string, 10000)
 
 	if *host == true {
 		server.StartServer()
-
 	}
 
 	rl.InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "pong")
 	defer rl.CloseWindow()
 
-	g := server.NewGame()
-	g.Client = client.NewClient()
+	client.ListeningClient(input, message)
 
 	rl.SetTargetFPS(60)
 	for !rl.WindowShouldClose() {
-		handleMovement(g)
-		if g.Ball.IsActive {
-			g.Ball.Position.X = g.Ball.Position.X + float32(g.Ball.Speed.X)
-			g.Ball.Position.Y = g.Ball.Position.Y + float32(g.Ball.Speed.Y)
-		} else {
-			g.Ball.Position = server.Coordinate{X: g.Red.Position.X + 2*g.Red.Size.X + g.Ball.Radius, Y: g.Red.Position.Y}
-		}
-		// ball.Position.Y = ball.Position.Y + float32(ball.Speed.Y)
-		if g.Ball.Position.X <= 0 {
-			g.Blue.Score = g.Blue.Score + 1
-			// reset
-			g.Reset()
-		}
-		if g.Ball.Position.X >= float32(SCREEN_WIDTH) {
-			g.Red.Score = g.Red.Score + 1
-			// reset
-			g.Reset()
-		}
 
-		if g.Ball.Position.Y <= 0 {
-			g.Ball.Speed.Y = 3.0
-		}
-		if g.Ball.Position.Y >= float32(SCREEN_HEIGHT) {
-			g.Ball.Speed.Y = -3.0
-		}
+		select {
+		case msg, ok := <-message:
+			if !ok {
+				log.Printf("there is something wrong with the recieved data\n")
 
-		if rl.CheckCollisionCircleRec(rl.Vector2(g.Ball.Position), g.Ball.Radius, rl.Rectangle{X: g.Red.Position.X, Y: g.Red.Position.Y - g.Red.Size.Y/2, Width: g.Red.Size.X, Height: g.Red.Size.Y}) {
-			g.Ball.Speed.X = 3.0
-			g.Ball.Speed.Y = (g.Ball.Position.Y - g.Red.Position.Y) / (g.Red.Size.Y / 2) * 5
+			}
+			err := json.Unmarshal(msg, g)
+			if err != nil {
+				log.Printf("error happend when recieved data from the server unmarshaled\n")
+			}
 
-		}
-		if rl.CheckCollisionCircleRec(rl.Vector2(g.Ball.Position), 10.0, rl.Rectangle{X: g.Blue.Position.X, Y: g.Blue.Position.Y - g.Blue.Size.Y/2, Width: g.Blue.Size.X, Height: g.Blue.Size.Y}) {
-			g.Ball.Speed.X = -3.0
-			g.Ball.Speed.Y = (g.Ball.Position.Y - g.Blue.Position.Y) / (g.Blue.Size.Y / 2) * 5
-		}
+		default:
+			handleMovement(input)
+			if rl.CheckCollisionCircleRec(rl.Vector2(g.Ball.Position), g.Ball.Radius, rl.Rectangle{X: g.Red.Position.X, Y: g.Red.Position.Y - g.Red.Size.Y/2, Width: g.Red.Size.X, Height: g.Red.Size.Y}) {
+				g.Ball.Speed.X = 3.0
+				g.Ball.Speed.Y = (g.Ball.Position.Y - g.Red.Position.Y) / (g.Red.Size.Y / 2) * 5
+				// send red collition with ball
 
-		// INFO: drawing start
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.Black)
-		rl.DrawRectangleRec(rl.Rectangle{X: g.Red.Position.X, Y: g.Red.Position.Y - (g.Red.Size.Y / 2), Width: g.Red.Size.X, Height: g.Red.Size.Y}, rl.Red)
-		rl.DrawRectangleRec(rl.Rectangle{X: g.Blue.Position.X, Y: g.Blue.Position.Y - (g.Blue.Size.Y / 2), Width: g.Blue.Size.X, Height: g.Blue.Size.Y}, rl.Blue)
-		rl.DrawCircleV(rl.Vector2(g.Ball.Position), 10.0, rl.White)
-		rl.DrawText(fmt.Sprintf("RED: %v |<=>| BLUE: %v", g.Red.Score, g.Blue.Score), int32(rl.GetScreenWidth()/2)-115, 0, 20, rl.White)
-		rl.EndDrawing()
+			}
+			if rl.CheckCollisionCircleRec(rl.Vector2(g.Ball.Position), 10.0, rl.Rectangle{X: g.Blue.Position.X, Y: g.Blue.Position.Y - g.Blue.Size.Y/2, Width: g.Blue.Size.X, Height: g.Blue.Size.Y}) {
+				g.Ball.Speed.X = -3.0
+				g.Ball.Speed.Y = (g.Ball.Position.Y - g.Blue.Position.Y) / (g.Blue.Size.Y / 2) * 5
+				// send blue collition with ball
+			}
+
+			// INFO: drawing start
+			rl.BeginDrawing()
+			rl.ClearBackground(rl.Black)
+			rl.DrawRectangleRec(rl.Rectangle{X: g.Red.Position.X, Y: g.Red.Position.Y - (g.Red.Size.Y / 2), Width: g.Red.Size.X, Height: g.Red.Size.Y}, rl.Red)
+			rl.DrawRectangleRec(rl.Rectangle{X: g.Blue.Position.X, Y: g.Blue.Position.Y - (g.Blue.Size.Y / 2), Width: g.Blue.Size.X, Height: g.Blue.Size.Y}, rl.Blue)
+			rl.DrawCircleV(rl.Vector2(g.Ball.Position), 10.0, rl.White)
+			rl.DrawText(fmt.Sprintf("RED: %v |<=>| BLUE: %v", g.Red.Score, g.Blue.Score), int32(rl.GetScreenWidth()/2)-115, 0, 20, rl.White)
+			rl.EndDrawing()
+		}
 	}
 }
 
-func handleMovement(g *server.Game) {
+func handleMovement(input chan string) {
 	if rl.IsKeyDown(rl.KeyJ) {
-		_, err := g.Client.Conn.Write([]byte("RKEY_J\n"))
-		if err != nil {
-			log.Println("pressing j is not sending data")
-		}
-		var res [256]byte
+		input <- "R_J"
+		// _, err := g.Client.Conn.Write([]byte("R_J"))
+		// if err != nil {
+		// 	log.Println("pressing j is not sending data")
+		// }
 
-		g.Client.Conn.ReadFromUDP(res[0:])
-		if err != nil {
-			log.Println("unable to get response for the server")
-		}
-		log.Printf("response from the server RKEY_J: %v", string(res[0:]))
-		var resVal server.Coordinate = g.Red.Position
-		err = json.Unmarshal(res[:], &resVal)
-		if err != nil {
-			log.Println(err.Error())
-		}
-		g.Red.Position = resVal
+		// var res []byte
+		//
+		// n, _, err := g.Client.Conn.ReadFromUDP(res[:])
+		// if err != nil {
+		// 	log.Println("unable to get response for the server")
+		// }
+		// log.Printf("response from the server RKEY_J: %v", string(res[0:n]))
+		//
+		// log.Printf("value: %v\n", string(res[:n]))
+		// var resval rl.Vector2
+		// err = json.Unmarshal(res[:n], &resval)
+		// if err != nil {
+		// 	log.Println("error form client:", err.Error())
+		// }
+		//
+		// g.Red.Position = resval
 	}
 	if rl.IsKeyDown(rl.KeyK) {
 		if g.Red.Position.Y > g.Red.Size.Y/2 {
@@ -116,12 +168,14 @@ func handleMovement(g *server.Game) {
 	}
 
 	if rl.IsKeyDown(rl.KeyA) {
+		// B_H
 		if g.Blue.Position.X > 0 {
 			g.Blue.Position.X = g.Blue.Position.X - 2
 		}
 	}
 
 	if rl.IsKeyDown(rl.KeyF) {
+		// B_L
 		if g.Blue.Position.X < float32(SCREEN_WIDTH)-g.Blue.Size.X {
 			g.Blue.Position.X = g.Blue.Position.X + 2
 		}
@@ -142,46 +196,46 @@ func handleMovement(g *server.Game) {
 	}
 }
 
-// func Reset() {
-// 	if g.Red.Score >= SCORE_LIMIT {
-// 		rl.BeginDrawing()
-// 		rl.ClearBackground(rl.Black)
-// 		rl.DrawText("RED WINS!!", int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 32, rl.Red)
-// 		rl.EndDrawing()
-// 		rl.WaitTime(2)
-// 		os.Exit(0)
-// 	}
-//
-// 	if g.Blue.Score >= SCORE_LIMIT {
-// 		rl.BeginDrawing()
-// 		rl.ClearBackground(rl.Black)
-// 		rl.DrawText("BLUE WINS!!", int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 32, rl.Blue)
-// 		rl.EndDrawing()
-// 		rl.WaitTime(2)
-// 		os.Exit(0)
-// 	}
-//
-// 	rl.BeginDrawing()
-// 	rl.ClearBackground(rl.Black)
-// 	rl.DrawText(fmt.Sprintf("RED: %v |<=>| BLUE: %v", g.Red.Score, g.Blue.Score), int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 24, rl.White)
-// 	rl.EndDrawing()
-// 	rl.WaitTime(2.5)
-//
-// 	// INFO: reset blue
-// 	g.Blue.Position.X = float32(SCREEN_WIDTH) - 10
-// 	g.Blue.Position.Y = 200
-// 	g.Blue.Size.X = 10
-// 	g.Blue.Size.Y = 100
-//
-// 	// INFO: reset red
-// 	g.Red.Position.X = 0
-// 	g.Red.Position.Y = 200
-// 	g.Red.Size.X = 10
-// 	g.Red.Size.Y = 100
-//
-// 	// INFO: reset ball
-// 	g.Ball.Position = rl.Vector2{X: g.Red.Position.X + 2*g.Red.Size.X + g.Ball.Radius, Y: g.Red.Position.Y}
-// 	g.Ball.Speed.Y = 0.0
-// 	g.Ball.Speed.X = 3.0
-// 	g.Ball.IsActive = false
-// }
+func Reset() {
+	if g.Red.Score >= SCORE_LIMIT {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+		rl.DrawText("RED WINS!!", int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 32, rl.Red)
+		rl.EndDrawing()
+		rl.WaitTime(2)
+		os.Exit(0)
+	}
+
+	if g.Blue.Score >= SCORE_LIMIT {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+		rl.DrawText("BLUE WINS!!", int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 32, rl.Blue)
+		rl.EndDrawing()
+		rl.WaitTime(2)
+		os.Exit(0)
+	}
+
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.Black)
+	rl.DrawText(fmt.Sprintf("RED: %v |<=>| BLUE: %v", g.Red.Score, g.Blue.Score), int32(rl.GetScreenWidth()/2)-115, int32(rl.GetScreenHeight()/2), 24, rl.White)
+	rl.EndDrawing()
+	rl.WaitTime(2.5)
+
+	// INFO: reset blue
+	g.Blue.Position.X = float32(SCREEN_WIDTH) - 10
+	g.Blue.Position.Y = 200
+	g.Blue.Size.X = 10
+	g.Blue.Size.Y = 100
+
+	// INFO: reset red
+	g.Red.Position.X = 0
+	g.Red.Position.Y = 200
+	g.Red.Size.X = 10
+	g.Red.Size.Y = 100
+
+	// INFO: reset ball
+	g.Ball.Position = rl.Vector2{X: g.Red.Position.X + 2*g.Red.Size.X + g.Ball.Radius, Y: g.Red.Position.Y}
+	g.Ball.Speed.Y = 0.0
+	g.Ball.Speed.X = 3.0
+	g.Ball.IsActive = false
+}
