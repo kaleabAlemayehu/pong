237b4ba36bffc179ecf356077d116f48e5b14d25
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-03-28 15:46:20 -0400

    feat: adding 2 client routine for continuous reading form the server and continous sending of data to the server when command is passed to input channel, and send new game state to the main thread by message channel

--- client/client.go
+++ client/client.go
@@ -9,15 +9,48 @@ type Client struct {
 	Conn *net.UDPConn
 }
 
-func NewClient() *Client {
+func ListeningClient(input chan string, msg chan []byte) {
 	udpAddr, err := net.ResolveUDPAddr("udp", "0.0.0.0:8000")
 	if err != nil {
-		log.Fatalf("client unable to resovle upd address Error: %v ", err.Error())
+		log.Printf("client unable to resovle upd address Error: %v ", err.Error())
 	}
 	conn, err := net.DialUDP("udp", nil, udpAddr)
 	if err != nil {
-		log.Fatalf("client unable to connect upd server Error: %v ", err.Error())
+		log.Printf("client unable to connect upd server Error: %v ", err.Error())
 	}
-	log.Println("connected succesfully to the server")
-	return &Client{Conn: conn}
+	log.Println("this does print out")
+	// listening routine from server
+	go func() {
+
+		res := make([]byte, 10240)
+		for {
+			n, _, err := conn.ReadFrom(res[:])
+			_ = n
+			if err != nil {
+				log.Printf("unable to start the server => Error:%v ", err.Error())
+			}
+			log.Printf("but this doesn't print out")
+			select {
+			case msg <- res[:n]: // Non-blocking send
+			default:
+				log.Println("Warning: Message dropped (buffer full)")
+			}
+		}
+	}()
+	// sending to server routine
+	go func() {
+		for {
+			select {
+			case i := <-input:
+				{
+					n, err := conn.Write([]byte(i))
+					if err != nil {
+						log.Printf("error while sending: %v", err.Error())
+					} else {
+						log.Printf("%d bytes sent to server\n", n)
+					}
+				}
+			}
+		}
+	}()
 }
